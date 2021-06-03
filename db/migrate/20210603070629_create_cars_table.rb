aca4d42982f55d554f42eabfe58e1c5d81391f48
Rianna <riannaxcastro@gmail.com>
2021-06-02 21:09:48 -1000

    created tables

---
+++ db/migrate/20210603070629_create_cars_table.rb
@@ -0,0 +1,10 @@
+class CreateCarsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.integer :year
+      t.string :make
+      t.string :model
+      t.integer :user_id
+    end
+  end
+end
