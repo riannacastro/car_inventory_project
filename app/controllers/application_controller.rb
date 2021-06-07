0f98adb37dd1f5bd0bd788810469f0f8dcef2148
Rianna <riannaxcastro@gmail.com>
2021-06-06 17:11:48 -1000

    added helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
 
 
 end
