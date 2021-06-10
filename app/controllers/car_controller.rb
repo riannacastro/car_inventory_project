e9d198b612ad9256abb92ec8c56783f1d84af01b
Rianna Castro <riannaxcastro@gmail.com>
2021-06-10 09:57:57 -1000

    added negations

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -3,17 +3,27 @@ require './config/environment'
 class CarController < ApplicationController
 
     get '/cars' do
+        if !logged_in?
+            redirect to '/login'
+        end
         @cars = Car.all 
         erb :'cars/index'
     end
 
 
     get '/cars/new' do
+        if !logged_in?
+            redirect to '/login'
+        end
+
         erb :'cars/new'
     end
 
 
     get '/cars/:id' do
+        if !logged_in?
+            redirect to '/login'
+        end
         @car = Car.find(params[:id])
 
         erb :'cars/show'
@@ -21,11 +31,17 @@ class CarController < ApplicationController
     
 
     get '/cars/:id/edit' do
+        if !logged_in?
+            redirect to '/login'
+        end
         @car = Car.find(params[:id])
         erb :'cars/edit'
     end
 
     post '/cars' do
+        if !logged_in?
+            redirect to '/login'
+        end
        car = Car.new(params)
        car.user = current_user
        car.save
@@ -33,12 +49,18 @@ class CarController < ApplicationController
     end
 
     patch '/cars/:id' do
+        if !logged_in?
+            redirect to '/login'
+        end
         @car = Car.find(params[:id])
         @car.update(params["car"])
         redirect :"cars/#{@car.id}"
     end
 
     delete '/cars/:id' do
+        if !logged_in?
+            redirect to '/login'
+        end
         @car = Car.find(params[:id])
         @car.destroy
         redirect :"/cars"
