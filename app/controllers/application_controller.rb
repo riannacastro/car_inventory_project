a98db0a1d4ee4df1e141b3d158e2699f648c1f95
Rianna <riannaxcastro@gmail.com>
2021-06-11 08:52:45 -1000

    added error file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, 'asdfgjkl'
-   
+    set :show_exceptions, false
+  end
+
+  not_found do
+    status 404
+     erb :error
   end
 
   get "/" do
